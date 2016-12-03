eab1cbdb4ea1caa31158273759c4f2df957f9bc5
Phil Miller <mille121@illinois.edu>
2016-12-03 11:24:40 -0600

    Registration, inheritance structure, etc)

--- base.cpp
+++ base.cpp
@@ -1,41 +1,74 @@
 #include <unordered_map>
 
 
+struct Chare
+{
+  static unsigned int type_registration_counter;
+
+  template<typename ChareType>
+  static void register_chare_type()
+  {
+    ++type_registration_counter;
+    ChareType::register_chare_members();
+  }
+  
+};
+
+unsigned int Chare::type_registration_counter = 0;
+
+template <typename ChareType>
+struct Proxy;
+
+template <typename ChareType>
+struct CBase : public Chare
+{
+  static std::unordered_map<void *, unsigned int> entry_methods;
+  static unsigned int registration_counter;
+  friend class Proxy<ChareType>;
+
+  template <typename Return, typename ...Args>
+  static void register_entry_method(Return (ChareType::*method_pointer)(Args...))
+  {
+    entry_methods[reinterpret_cast<void*>(method_pointer)] = ++registration_counter;
+  }
+
+
+};
+
 template <typename ChareType>
 struct Proxy
 {
   int collection;
   int element;
-
   
   template<typename ...Args>
   void operator()(void (ChareType::*entry_method)(Args...), Args... args)
   {
-    int ep = ChareType::entry_methods[reinterpret_cast<void*>(entry_method)];
+    int ep = CBase<ChareType>::entry_methods[reinterpret_cast<void*>(entry_method)];
 
   }
   
 };
 
-class Chare
-{
-  static std::unordered_map<void *, unsigned int> entry_methods;
-  friend class Proxy<Chare>;
 
-public:
+struct MyChare : public CBase<MyChare>
+{
+
   void foo(int i) { }
 
+  static void register_chare_members()
+  {
+    register_entry_method(&MyChare::foo);
+  }
 
 };
 
-
-
 int main(int argc, char** argv)
 {
-  Chare c;
+  MyChare c;
 
-  Proxy<Chare> p{10,10};
-  p(&Chare::foo, 1);
+  Proxy<MyChare> p{10,10};
+  p(&MyChare::foo, 1);
   
 
   return 0;
